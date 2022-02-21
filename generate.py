08e6b9a28bcdc9dcb6b9f60ba063e90fc600a14a
Laddge <dev.laddge@gmail.com>
2022-02-22 07:48:37 +0900

    [feat] update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-import textwrap
 
 # 転移学習済みモデル
 MODEL_DIR = "./model"
@@ -21,8 +20,6 @@ if USE_GPU:
 MAX_SOURCE_LENGTH = 64   # 入力される記事本文の最大トークン数
 MAX_TARGET_LENGTH = 64  # 生成されるタイトルの最大トークン数
 
-say = input()
-
 
 def normalize_text(text):
     assert "\n" not in text and "\r" not in text
@@ -37,37 +34,37 @@ def preprocess(text):
     return normalize_text(text.replace("\n", ""))
 
 
-# 推論モード設定
-trained_model.eval()
+def generate(text):
+    # 推論モード設定
+    trained_model.eval()
 
-# 前処理とトークナイズを行う
-inputs = [preprocess(say)]
-batch = tokenizer.batch_encode_plus(
-    inputs, max_length=MAX_SOURCE_LENGTH, truncation=True,
-    padding="longest", return_tensors="pt")
+    # 前処理とトークナイズを行う
+    inputs = [preprocess(text)]
+    batch = tokenizer.batch_encode_plus(
+        inputs, max_length=MAX_SOURCE_LENGTH, truncation=True,
+        padding="longest", return_tensors="pt")
 
-input_ids = batch['input_ids']
-input_mask = batch['attention_mask']
-if USE_GPU:
-    input_ids = input_ids.cuda()
-    input_mask = input_mask.cuda()
+    input_ids = batch['input_ids']
+    input_mask = batch['attention_mask']
+    if USE_GPU:
+        input_ids = input_ids.cuda()
+        input_mask = input_mask.cuda()
 
-# 生成処理を行う
-outputs = trained_model.generate(
-    input_ids=input_ids, attention_mask=input_mask,
-    max_length=MAX_TARGET_LENGTH,
-    # temperature=1.0,  # 生成にランダム性を入れる温度パラメータ
-    # num_beams=10,  # ビームサーチの探索幅
-    # diversity_penalty=1.0,  # 生成結果の多様性を生み出すためのペナルティパラメータ
-    # num_beam_groups=10,  # ビームサーチのグループ
-    # num_return_sequences=10,  # 生成する文の数
-    repetition_penalty=8.0,   # 同じ文の繰り返し（モード崩壊）へのペナルティ
-)
+    # 生成処理を行う
+    outputs = trained_model.generate(
+        input_ids=input_ids, attention_mask=input_mask,
+        max_length=MAX_TARGET_LENGTH,
+        # temperature=1.0,  # 生成にランダム性を入れる温度パラメータ
+        # num_beams=10,  # ビームサーチの探索幅
+        # diversity_penalty=1.0,  # 生成結果の多様性を生み出すためのペナルティパラメータ
+        # num_beam_groups=10,  # ビームサーチのグループ
+        # num_return_sequences=10,  # 生成する文の数
+        repetition_penalty=8.0,   # 同じ文の繰り返し（モード崩壊）へのペナルティ
+    )
 
-# 生成されたトークン列を文字列に変換する
-generated_replies = [tokenizer.decode(
-    ids, skip_special_tokens=True, clean_up_tokenization_spaces=False) for ids in outputs]
+    # 生成されたトークン列を文字列に変換する
+    generated_replies = [tokenizer.decode(
+        ids, skip_special_tokens=True, clean_up_tokenization_spaces=False) for ids in outputs]
 
-# 生成された文章を表示する
-for i, reply in enumerate(generated_replies):
-    print("\n".join(textwrap.wrap(f"{i+1:2}. {reply}")))
+    # 生成された文章を返す
+    return "\n".join(generated_replies)
